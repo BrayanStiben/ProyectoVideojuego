afc178e5f99922695cfb2ed9f193468b7b646f98
Brayan stiben meza <brayans.mezac@uqvirtual.edu.co>
2026-03-19 19:18:34 -0500

    Codigo inicial

--- app/src/main/java/com/example/juegopedido/MainActivity.kt
+++ app/src/main/java/com/example/juegopedido/MainActivity.kt
@@ -25,6 +25,7 @@ class MainActivity : ComponentActivity() {
                 ) {
                     // AQUÍ LLAMAMOS A TU PANTALLA
                     JuegoMemoriaScreen()
+
                 }
             }
         }
